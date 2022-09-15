27a5ee102a24f71e9aede89427424c9f64de414e
Vyacheslav Lazarenko <lasarenko91@mail.ru>
2022-09-15 21:47:45 +0300

    fx test_add_new_book_add_two_same_books_without_error

--- tests.py
+++ tests.py
@@ -23,7 +23,9 @@ class TestBooksCollector:
 
     def test_add_new_book_add_two_same_books_without_error(self):
         collector = BooksCollector()
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Гордость и предубеждение и зомби')
+
+        same_name = 'Гордость и предубеждение и зомби'
+        collector.add_new_book(same_name)
+        collector.add_new_book(same_name)
 
         assert len(collector.get_books_rating()) == 1
